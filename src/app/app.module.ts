460ec6e858162a3d6bfaa25cf5caccbe754d1852
StefanoVe <stefano.vecchietti.99@gmail.com>
2022-12-02 23:10:31 +0100

    avatar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorsModule } from './errors/errors.module';
+import { SharedModule } from './shared/shared.module';
 import { WidgetsModule } from './widgets/widgets.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, WidgetsModule, ErrorsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    WidgetsModule,
+    ErrorsModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultBackgroundComponent } from './components/default-background/default-background.component';
+import { WallpaperComponent } from './components/wallpaper/wallpaper.component';
 
 
 
 @NgModule({
   declarations: [
-    DefaultBackgroundComponent
+    DefaultBackgroundComponent,
+    WallpaperComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    DefaultBackgroundComponent
+    DefaultBackgroundComponent,
+    WallpaperComponent
   ]
 })
 export class SharedModule { }
