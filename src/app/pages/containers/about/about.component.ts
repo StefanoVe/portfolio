0d62db50fd54af489855c0ace25b89967d7053ec
StefanoVe <stefano.vecchietti.99@gmail.com>
2022-12-14 13:29:22 +0100

    minor changes

--- src/app/pages/containers/about/about.component.ts
+++ src/app/pages/containers/about/about.component.ts
@@ -39,15 +39,15 @@ export class AboutComponent {
 
   public timeOffTxt() {
     return this._lang._languageSensitiveText({
-      en: "In my time off i'm an airsoft afficionado and an incredible Geek.",
-      it: 'Nel tempo libero sono un appassionato di Softair e un Geek incredibile.',
+      en: "Last but not Least, in my time off i'm an airsoft afficionado and an incredible Geek.",
+      it: 'Ultimo ma non per importanza, nel tempo libero sono un appassionato di Softair e un Geek incredibile.',
     });
   }
 
   public pastExperiencesTxt() {
     return this._lang._languageSensitiveText({
       en: "I started coding roughly at 14 years old, using basic C# to write and edit Mod Menus and RTMs in the PS3 modding scene. I first approached JavaScript at 20 years old, having to build the public website and members' dashboard for Legione Etruria ASD. Then, at 22 years old, i got my first full time job as a full stack web developer, working for Golden Italia.",
-      it: 'Ho iniziato a scrivere codice a circa 14 anni, usando C# di base per scrivere e modificare Mod Menus e RTMs nella scena modding PS3. Mi sono avvicinato per la prima volta a JavaScript a 20 anni, dovendo sviluppare un sito pubblico e una dashboard per i membri di Legione Etruria ASD. A 22 anni invece, ho trovato il mio primo lavoro dipendente come sviluppatore full stack web, lavorando per Golden Italia.',
+      it: 'Ho iniziato a scrivere codice a circa 14 anni, usando C# di base per scrivere e modificare Mod Menus e RTMs nella scena modding PS3. Mi sono avvicinato per la prima volta a JavaScript a 20 anni, dovendo sviluppare un sito pubblico e una dashboard membri per Legione Etruria ASD. A 22 anni invece, ho trovato il mio primo lavoro dipendente come sviluppatore full stack web, lavorando per Golden Italia.',
     });
   }
 
